2df74edf688495e8829f587a923b34d0aed37cf9
@ <@>
2020-04-21 21:51:30 -0500

    more install tweaks

--- dnsdumpster/install.sh
+++ dnsdumpster/install.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 if [ ! -d dnsdmpstr ]; then
     git clone https://github.com/zeropwn/dnsdmpstr.git $SCRIPTPATH/dnsdmpstr
-    touch dnsdmpstr/__init__.py
+    touch $SCRIPTPATH/dnsdmpstr/__init__.py
 fi
