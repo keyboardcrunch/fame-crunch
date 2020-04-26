d42a286a9155d68088d31bbc79549e7ae76d1c7c
keyboardcrunch <>
2020-04-26 15:20:44 -0500

    Making progress on crt.sh

--- processing/certsh/crtsh.py
+++ processing/certsh/crtsh.py
@@ -1,15 +1,23 @@
 import os
 import sys
 import json
+import requests
 
 from fame.core.module import ProcessingModule
-from fame.common.exceptions import ModuleInitializationError
+from fame.common.utils import tempdir
+from fame.common.exceptions import ModuleInitializationError, ModuleExecutionError
 
 try:
-    import pycrtsh
-    has_pycrtsh = True
+    import tldextract
+    has_requests = True
 except ImportError:
-    has_pycrtsh = False
+    has_requests = False
+
+try:
+    import tldextract
+    has_tldextract = True
+except ImportError:
+    has_tldextract = False
 
 class Crtsh(ProcessingModule):
     name = "crt.sh"
@@ -18,27 +26,60 @@ class Crtsh(ProcessingModule):
 
     config = [
         {
-            'name': 'reverse_dns',
+            'name': 'save_json',
             'type': 'bool',
             'default': True,
-            'description': 'List reverse DNS entries from DnsDumpster.'
+            'description': 'Save certificate information as json output.'
         },
         {
-            'name': 'page_links',
+            'name': 'save_hosts',
             'type': 'bool',
             'default': True,
-            'description': 'Grab page links using HackerTarget.'
-        },
-        {
-            'name': 'http_headers',
-            'type': 'bool',
-            'default': True,
-            'description': 'Grab HTTP server headers from URL using HackerTarget.'
+            'description': 'Save found hostnames in a file list.'
         },
     ]
 
     def initialize(self):
-        if not has_dnsdump:
-            raise ModuleInitializationError(self, "Missing dependancy: dnsdmpstr")
+        if not has_requests:
+            raise ModuleInitializationError(self, "Missing dependancy: requests")
         if not has_tldextract:
             raise ModuleInitializationError(self, "Missing dependancy: tldextract")
+
+    def each(self, target):
+        self.results = {}
+        tmpdir = tempdir()
+
+        # Get the root domain
+        url = target
+        domain = tldextract.extract(url)
+        root_domain = domain.domain + '.' + domain.suffix
+
+        self.log("debug", "Querying crt.sh with root domain...")
+        try:
+            req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=root_domain))
+            json_data = json.loads(req.text)
+        else:
+            raise ModuleExecutionError("Failed to get data from crt.sh")
+        
+        # We want to gather data in a way to present the details.html template as default
+        # 
+        
+
+        # Save JSON data
+        if self.save_json:
+            self.log("debug", "Saving json output from crt.sh...")
+            json_file = "{r}.json".format(r=root_domain)
+            with open(os.path.join(tempdir, json_file), "w") as jf:
+                jf.write(json.dumps(json_data, indent=4))
+                jf.close()
+
+        # Save host list
+        if self.save_hosts:
+            self.log("debug", "Saving host list...")
+            host_file = "{r}-hostlist.txt".format(r=root_domain)
+            with open(os.path.join(tempdir, host_file), "w") as hf:
+                for (key,value) in enumerate(json_data):
+                    hf.write("{hn}\r\n".format(hn=value['name_value']))
+                hf.close()
+
+        self.log("debug","Crt.sh finished.")
