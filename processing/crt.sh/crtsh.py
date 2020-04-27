6e30381706be1597882e00d35d102e6db02cd0ed
@ <@>
2020-04-26 19:10:35 -0500

    finished but needs testing?

--- processing/crt.sh/crtsh.py
+++ processing/crt.sh/crtsh.py
@@ -36,6 +36,12 @@ class Crtsh(ProcessingModule):
             'default': True,
             'description': 'Save found hostnames in a file list.'
         },
+        {
+            'name': 'cert_count',
+            'type': 'int',
+            'default': 10,
+            'description': 'Number of most recent certs to show in report.'
+        },
     ]
 
     def initialize(self):
@@ -45,8 +51,9 @@ class Crtsh(ProcessingModule):
             raise ModuleInitializationError(self, "Missing dependancy: tldextract")
 
     def each(self, target):
-        self.results = {}
         tmpdir = tempdir()
+        self.results = {}
+        self.results['cert_count'] = self.cert_count
 
         # Get the root domain
         url = target
@@ -57,28 +64,44 @@ class Crtsh(ProcessingModule):
         try:
             req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=root_domain))
             json_data = json.loads(req.text)
-        else:
+        except:
             raise ModuleExecutionError("Failed to get data from crt.sh")
         
         # We want to gather data in a way to present the details.html template as default
-        # 
-
+        vc = 0
+        certs = []
+        for (key, value) in enumerate(json_data): # Add first X cert dicts to a list
+            while vc < self.cert_count:
+                certs.append(value)
+                vc += 1
+        self.results['top_certs'] = certs
 
         # Save JSON data
         if self.save_json:
             self.log("debug", "Saving json output from crt.sh...")
             json_file = "{r}.json".format(r=root_domain)
-            with open(os.path.join(tempdir, json_file), "w") as jf:
-                jf.write(json.dumps(json_data, indent=4))
-                jf.close()
+            json_save = os.path.join(tempdir, json_file)
+            try:
+                with open(json_save, "w") as jf:
+                    jf.write(json.dumps(json_data, indent=4))
+                    jf.close()
+                self.add_extracted_file(json_save)
+            except:
+                raise ModuleExecutionError("Failed to save json data from crt.sh")
 
         # Save host list
         if self.save_hosts:
             self.log("debug", "Saving host list...")
             host_file = "{r}-hostlist.txt".format(r=root_domain)
-            with open(os.path.join(tempdir, host_file), "w") as hf:
-                for (key,value) in enumerate(json_data):
-                    hf.write("{hn}\r\n".format(hn=value['name_value']))
-                hf.close()
+            host_save = os.path.join(tempdir, host_file)
+            try:
+                with open(host_save, "w") as hf:
+                    for (key, value) in enumerate(json_data):
+                        hf.write("{hn}\r\n".format(hn=value['name_value']))
+                    hf.close()
+                self.add_extracted_file(host_save)
+            except:
+                raise ModuleExecutionError("Failed to save json data from crt.sh")
 
-        self.log("debug","Crt.sh finished.")
+        self.log("debug","crt.sh finished.")
+        return True
