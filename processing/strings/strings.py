13d8757d8cc5b1d34d3a3a23edb05febc622d17b
keyboardcrunch <>
2020-11-09 19:51:18 -0600

    fix

--- processing/strings/strings.py
+++ processing/strings/strings.py
@@ -1,11 +1,13 @@
 import os
-from fame.common.utils import tempdir
+
+
 from fame.core.module import ProcessingModule
+from fame.common.utils import tempdir
 from fame.common.exceptions import ModuleInitializationError
 
 from ..docker_utils import HAVE_DOCKER, docker_client, temp_volume
 
-class Strings(ProcessingModule):
+class StringFile(ProcessingModule):
     name = "strings"
     description = "Strings file analysis."
     acts_on = ["executable","pdf","txt","rtf","vbscript","javascript","html","dll"]
@@ -27,16 +29,14 @@ class Strings(ProcessingModule):
         )
 
     def each(self, target):
-        self.results = {
-            'strings': u''
-        }
+        self.results = {}
 
         self.outdir = temp_volume(target)
         results_dir = os.path.join(self.outdir, "output")
 
         self.run_strings(os.path.basename(target))
 
-        with open(os.path.join(results_dir, "results.txt")) as results_text:
-            self.results["strings"] = results_text
+        with open(os.path.join(results_dir, "results.txt"), "r") as results_text:
+            self.results['strings'] = results_text.readlines()
         
-        return len(self.results["strings"]) > 0
+        return len(self.results['strings']) > 0
